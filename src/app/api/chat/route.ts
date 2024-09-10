d5b87b49dab91fac58cdf9e8007899852ea464eb
Pavitar Singh <71433170+Pavitar01@users.noreply.github.com>
2024-09-10 16:44:58 +0530

    Feature: Update response for route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,5 @@
 import Pusher from "pusher";
+
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID as string,
   key: process.env.PUSHER_KEY as string,
@@ -6,12 +7,14 @@ const pusher = new Pusher({
   cluster: process.env.PUSHER_CLUSTER as string,
   useTLS: true,
 });
+
 export async function POST(request: any) {
-  const { message } = await request.json();
-  pusher
-    .trigger("chat-channel", "new-message", { message })
-    .then(() => {
-      return new Response("Message sent", { status: 200 });
-    })
-    .catch((error) => console.error("Error sending message:", error));
+  try {
+    const { message } = await request.json();
+    await pusher.trigger("chat-channel", "new-message", { message });
+    return new Response("Message sent", { status: 200 });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
