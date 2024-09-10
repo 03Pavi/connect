6297cc50537883987506cd00626c6913c6e8065f
Pavitar Singh <71433170+Pavitar01@users.noreply.github.com>
2024-09-10 16:25:27 +0530

    Feature: Update Pusher arguments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,9 @@ import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
+  forceTLS: true,
 });
-
+Pusher.logToConsole = true;
 export default function Home() {
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<string[]>([]);
